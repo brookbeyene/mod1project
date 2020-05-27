8e2a23557d4ccce2ab653abb022334432e9ff170
Brook <brookbeyene37@gmail.com>
2020-05-27 16:14:30 -0600

    add s

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require 'sinatra/activerecord'
 Bundler.require
 
 require_all 'lib'
-binding.pry
+# binding.pry
 # connection_details = YAML::load(File.open('config/database.yml'))
 #ActiveRecord::Base.establish_connection(connection_details)
--- db/migrate/20200527174937_create_student_table.rb
+++ db/migrate/20200527174937_create_student_table.rb
@@ -1,6 +1,6 @@
 class CreateStudentTable < ActiveRecord::Migration[6.0]
   def change
-    create_table :student do |t|
+    create_table :students do |t|
       t.string :name
       t.text :track
        
